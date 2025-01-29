4772dec7d3dedfa3af469209059c49c8adb7a681
LaBani Said <labani.said@outlook.com>
2025-01-29 11:57:06 +0100

    Add Individual.cs

---
+++ src/Core/Individuals/Individual.cs
@@ -0,0 +1,12 @@
+﻿namespace GildedRoseKata.Individuals;
+
+public class Individual
+{
+    public required string FirstName { get; init; }
+
+    public required string LastName { get; init; }
+
+    public required DateOnly BirthDate { get; init; }
+
+    public required string Nationality { get; init; }
+}
